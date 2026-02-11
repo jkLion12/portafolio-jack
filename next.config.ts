26e60fe99e4698e73e1820a36a190ea61f5dbe8d
Jack Huamani <jackhuamani25@gmail.com>
2026-02-10 20:06:12 -0500

    para subir a git pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/nombre-portafolio-jack" : "",
+
+  images: {
+    unoptimized: true, // Necesario para exportación estática en GitHub Pages
+  },
 };
 
 export default nextConfig;
